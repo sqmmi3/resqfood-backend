64806f4dced2f9de02ee719fc0548d23c10dab9e
Thomas Bosmans <thomas.bosmans@student.ucll.be>
2025-11-04 14:39:41 +0100

    basic relationship between item and user

--- ucll/ucll/src/main/java/be/ucll/model/Item.java
+++ ucll/ucll/src/main/java/be/ucll/model/Item.java
@@ -1,7 +1,15 @@
 package be.ucll.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
 public class Item {
 
     public enum Type{
@@ -15,15 +23,28 @@ public class Item {
         OTHER
     }
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
     private LocalDate expirationDate;
     private Type type;
 
+    @ManyToMany(mappedBy = "items")
+    private List<User> users;
+
+    protected Item() {
+    }
+
     public Item(String name, LocalDate expirationDate, Type type) {
         this.name = name;
         this.expirationDate = expirationDate;
         this.type = type;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
--- ucll/ucll/src/main/java/be/ucll/model/User.java
+++ ucll/ucll/src/main/java/be/ucll/model/User.java
@@ -1,15 +1,46 @@
 package be.ucll.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String username;
     private String email;
     private String password;
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_items",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items;
+
+    protected User() {
+    }
 
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.items = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getUsername() {
@@ -34,5 +65,17 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        this.items.remove(item);
     }
 }
