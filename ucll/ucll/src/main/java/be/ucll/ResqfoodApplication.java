b5f333f6dcf6377a5f303e57c4d5e1de7d97ef79
Thomas Bosmans <thomas.bosmans@student.ucll.be>
2025-11-04 13:01:43 +0100

    project setup

---
+++ ucll/ucll/src/main/java/be/ucll/ResqfoodApplication.java
@@ -0,0 +1,13 @@
+package be.ucll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResqfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResqfoodApplication.class, args);
+	}
+
+}
---
+++ ucll/ucll/src/test/java/be/ucll/ResqfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResqfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
