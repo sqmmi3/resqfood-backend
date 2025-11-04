72c5ede96a5219bef8308017e057e976783c8c6b
Thomas Bosmans <thomas.bosmans@student.ucll.be>
2025-11-04 14:16:06 +0100

    created folder for repo, serv, contr and model

--- ucll/ucll/src/main/java/be/ucll/ResqfoodApplication.java
+++ ucll/ucll/src/main/java/be/ucll/ResqfoodApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ResqfoodApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ResqfoodApplication.class, args);
 	}
-
 }
