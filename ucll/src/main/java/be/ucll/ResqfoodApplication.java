5fcfd50f8949710c18173a37765a8d97e8fee1d6
sqmmi3 <samip.shrestha@student.ucll.be>
2025-11-26 15:28:44 +0100

    FEAT [main]: 1. JWT_SECRET and JWT_EXPIRATION fixed 2. ResQUserDetailsService.java with @Service annotation

--- ucll/src/main/java/be/ucll/ResqfoodApplication.java
+++ ucll/src/main/java/be/ucll/ResqfoodApplication.java
@@ -8,13 +8,15 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class ResqfoodApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
+		Dotenv dotenv = Dotenv.configure().directory("mobile-apps-team-11/backend/ucll").load();
 
 		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
 		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
 		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
 		System.setProperty("DB_USER", dotenv.get("DB_USER"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
 
 		SpringApplication.run(ResqfoodApplication.class, args);
 	}
--- ucll/src/main/java/be/ucll/service/ResQUserDetailsService.java
+++ ucll/src/main/java/be/ucll/service/ResQUserDetailsService.java
@@ -3,10 +3,12 @@ package be.ucll.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import be.ucll.model.User;
 import be.ucll.repository.UserRepository;
 
+@Service
 public class ResQUserDetailsService implements UserDetailsService {
   
   private final UserRepository userRepository;
