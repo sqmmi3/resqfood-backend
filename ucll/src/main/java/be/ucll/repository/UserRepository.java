47e463314c04a9bae6ee0403248c29c5250b2ff9
sqmmi3 <samip.shrestha@student.ucll.be>
2025-11-30 23:52:46 +0100

    FEAT [registerDTO]: registering route with DTO and validations

--- ucll/src/main/java/be/ucll/repository/UserRepository.java
+++ ucll/src/main/java/be/ucll/repository/UserRepository.java
@@ -9,4 +9,6 @@ import be.ucll.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUsername(String username);
   Optional<User> findByEmail(String email);
+  boolean existsByUsername(String username);
+  boolean existsByEmail(String email);
 }
