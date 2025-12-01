6a57c25e1be478c108b7d25b770a4f95de0dc254
sqmmi3 <samip.shrestha@student.ucll.be>
2025-12-01 15:43:35 +0100

    FIX [dev]: bean error for Test class with JwtService fixed; Test class had missing jwt-values, Test class does not look into .env

--- ucll/src/test/java/be/ucll/ResqfoodApplicationTests.java
+++ ucll/src/test/java/be/ucll/ResqfoodApplicationTests.java
@@ -3,9 +3,14 @@ package be.ucll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+	"jwt.secret=test_secret_1234567890_test_secret_key",
+	"jwt.expiration=3600000"
+})
 class ResqfoodApplicationTests {
 
 	@Test
