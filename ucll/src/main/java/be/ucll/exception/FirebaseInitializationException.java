bad55a8fef0aea2ead136058a1671c785ca8227c
sqmmi3 <samip.shrestha@student.ucll.be>
2025-12-07 21:01:18 +0100

    FEAT [notification]: 1. NotificationScheduler for the expiration time 2. NotificationService to push notification to device 3. UserService: added addDeviceToken to determine to which device to send token to for which user, added removeDeviceToken on logout, added getDeviceToken for list of all device tokens of user 4. UserItem: added lastNotifiedAt field to handle du[lication 5. FirebaseConfig for firebaseMessaging that handles notification templates 6. User: added deviceTokens field

---
+++ ucll/src/main/java/be/ucll/exception/FirebaseInitializationException.java
@@ -0,0 +1,7 @@
+package be.ucll.exception;
+
+public class FirebaseInitializationException extends RuntimeException {
+  public FirebaseInitializationException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
