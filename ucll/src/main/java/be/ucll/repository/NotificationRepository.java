d6769422bcc76de3501d317c47e8eda47d5c34e7
Tomas Vaneynde <tvaneynde@users.noreply.github.com>
2025-12-28 18:17:20 +0100

    Added markAllAsRead query

--- ucll/src/main/java/be/ucll/repository/NotificationRepository.java
+++ ucll/src/main/java/be/ucll/repository/NotificationRepository.java
@@ -3,6 +3,9 @@ package be.ucll.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import be.ucll.model.Notification;
 
@@ -11,4 +14,8 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     List<Notification> findByUser_UsernameOrderByTimestampDesc(String username);
 
     long countByUser_UsernameAndIsReadFalse(String username);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.isRead = true WHERE n.user.username = :username AND n.isRead = false")
+    void markAllAsRead(@Param("username") String username);
 }
